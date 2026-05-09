6149089b2d0015cb1603e9dceb69ac1f5ebf665a
alexander <tchebaeff.alexander@yandex.ru>
2026-05-09 14:06:13 +0500

    логика сохранения моделей в csv файла и тесты

--- src/main/java/org/writer/Main.java
+++ src/main/java/org/writer/Main.java
@@ -1,7 +1,50 @@
 package org.writer;
 
+import org.writer.csv.CsvWriter;
+import org.writer.model.Months;
+import org.writer.model.Person;
+import org.writer.model.Student;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
+/**
+ * Демонстрирует экспорт объектов модели в CSV с помощью {@link Writable}.
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) throws IOException {
+        Path outDir = Path.of(System.getProperty("user.dir"), "output");
+        java.nio.file.Files.createDirectories(outDir);
+
+        Writable csv = new CsvWriter();
+
+        List<Person> people = List.of(
+                Person.builder()
+                        .firstName("Anna \"Hi\" ")
+                        .lastName("Ivanova, test")
+                        .dayOfBirth(15)
+                        .monthOfBirth(Months.MARCH)
+                        .yearOfBirth(2001)
+                        .build(),
+                Person.builder()
+                        .firstName("Boris")
+                        .lastName("Petrov")
+                        .dayOfBirth(3)
+                        .monthOfBirth(Months.NOVEMBER)
+                        .yearOfBirth(1998)
+                        .build()
+        );
+        String peopleFile = outDir.resolve("people.csv").toString();
+        csv.writeToFile(people, peopleFile);
+        System.out.println("Wrote " + peopleFile);
+
+        List<Student> students = List.of(
+                Student.builder().name("Elena Smirnova").score(List.of("5", "4", "5")).build(),
+                Student.builder().name("Dmitry Kozlov").score(List.of("3", "4", "4", "5")).build()
+        );
+        String studentsFile = outDir.resolve("students.csv").toString();
+        csv.writeToFile(students, studentsFile);
     }
 }
--- src/main/java/org/writer/model/Student.java
+++ src/main/java/org/writer/model/Student.java
@@ -3,15 +3,23 @@ package org.writer.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.writer.csv.CsvColumn;
 
 import java.util.List;
 
+/**
+ * Сущность Студента
+ */
 @Data
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class Student {
 
+    @CsvColumn(order = 1, name = "Name")
     private String name;
 
+    @CsvColumn(order = 2, name = "Scores")
     private List<String> score;
 }
